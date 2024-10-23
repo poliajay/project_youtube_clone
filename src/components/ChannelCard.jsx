9790bc8150bc8fef3c3ec7c4db6312c18d0b00c2
Iam-p <poliajayazure@gmail.com>
2024-10-23 18:28:21 +0530

    Update ChannelCard.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -8,7 +8,7 @@ const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={{
       boxShadow: 'none',
-      borderRadius: '20px',
+      borderRadius: '40px',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
